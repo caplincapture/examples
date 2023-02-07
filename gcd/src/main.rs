5c2f029f31c4221090c4f6e40cb73c6974f8499e
caplincapture <leuping@gmail.com>
2023-02-07 10:41:22 -0800

    gcd attempt

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -29,10 +29,6 @@ use std::env;
 fn main() {
     let mut numbers = Vec::new();
 
-    for arg in env::args().skip(1) {
-        numbers.push(u64::from_str(&arg)
-                     .expect("error parsing argument"));
-    }
 
     if numbers.len() == 0 {
         eprintln!("Usage: gcd NUMBER ...");
@@ -41,7 +37,6 @@ fn main() {
 
     let mut d = numbers[0];
     for m in &numbers[1..] {
-        d = gcd(d, *m);
     }
 
     println!("The greatest common divisor of {:?} is {}",
