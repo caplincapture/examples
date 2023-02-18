561c121209d3c2cc474228d3b0fb9f75691a51a8
caplincapture <leuping@gmail.com>
2023-02-18 13:30:28 -0800

    finish factory and start adapter

---
+++ factorymethod/target/debug/deps/factorymethod-b7f0a00431b40252.d
@@ -0,0 +1,9 @@
+/home/g/Desktop/examples/factorymethod/target/debug/deps/factorymethod-b7f0a00431b40252.rmeta: src/main.rs src/gui.rs src/html_gui.rs src/init.rs src/windows_gui.rs
+
+/home/g/Desktop/examples/factorymethod/target/debug/deps/factorymethod-b7f0a00431b40252.d: src/main.rs src/gui.rs src/html_gui.rs src/init.rs src/windows_gui.rs
+
+src/main.rs:
+src/gui.rs:
+src/html_gui.rs:
+src/init.rs:
+src/windows_gui.rs:
---
+++ factorymethod/target/debug/deps/factorymethod-df134c89646124e3.d
@@ -0,0 +1,9 @@
+/home/g/Desktop/examples/factorymethod/target/debug/deps/factorymethod-df134c89646124e3.rmeta: src/main.rs src/gui.rs src/html_gui.rs src/init.rs src/windows_gui.rs
+
+/home/g/Desktop/examples/factorymethod/target/debug/deps/factorymethod-df134c89646124e3.d: src/main.rs src/gui.rs src/html_gui.rs src/init.rs src/windows_gui.rs
+
+src/main.rs:
+src/gui.rs:
+src/html_gui.rs:
+src/init.rs:
+src/windows_gui.rs:
