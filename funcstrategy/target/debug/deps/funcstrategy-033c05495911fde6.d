5149b965997b6a0d14aaa6cb87ef2d7cae40570c
g <leuping@gmail.com>
2023-03-12 09:07:22 -0700

    added func strategy

---
+++ funcstrategy/target/debug/deps/funcstrategy-033c05495911fde6.d
@@ -0,0 +1,5 @@
+/home/g/Desktop/examples/funcstrategy/target/debug/deps/funcstrategy-033c05495911fde6.rmeta: src/main.rs
+
+/home/g/Desktop/examples/funcstrategy/target/debug/deps/funcstrategy-033c05495911fde6.d: src/main.rs
+
+src/main.rs:
---
+++ funcstrategy/target/debug/deps/funcstrategy-ec2f663cf8846f82.d
@@ -0,0 +1,5 @@
+/home/g/Desktop/examples/funcstrategy/target/debug/deps/funcstrategy-ec2f663cf8846f82.rmeta: src/main.rs
+
+/home/g/Desktop/examples/funcstrategy/target/debug/deps/funcstrategy-ec2f663cf8846f82.d: src/main.rs
+
+src/main.rs:
